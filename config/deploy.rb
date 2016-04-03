21b1b08986c3c8d1325e70719a04976be9017136
sunjewelde <shibamuran@gmail.com>
2016-04-03 00:01:35 +0000

    test2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,18 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'myapp/code3'
+set :application, 'code3'
 set :repo_url, 'https://github.com/sunjewelde/diamond-b5.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/myapp/code3'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -30,7 +31,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rvm_ruby_version, '2.2.1'
 
-set :passenger_restart_with_touch, true
+# set :passenger_restart_with_touch, true
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
