51a319886405e6bf6b1227e6e32628ae082557ba
sunjewelde <shibamuran@gmail.com>
2016-04-13 21:06:54 +0000

    test

--- app/models/diamond.rb
+++ app/models/diamond.rb
@@ -106,7 +106,8 @@ class Diamond <ActiveRecord::Base
        
     # diamond.save
     
-    CSV.foreach(file.path, headers: true) do |row|
+    # CSV.foreach(file.path, headers: true) do |row|
+  CSV.foreach(file.path) do |row|
   Diamond.create(:date => row[0], :weight => row[4], :color => row[5], :clar => row[9], :length => row[10], :width => row[11],
                  :depth => row[12], :cut_grade => row[14], :rapnet_list_price => row[16], :rapnet_discount => row[17], :price_per_carat => row[18], 
                 :polish => row[26], :symmetry => row[27], :fluorescen => row[28], :certificate_id => row[37], :end_price => row[48])
