2007e379e538b69a7330d81181f5941a3e113a3b
sunjewelde <shibamuran@gmail.com>
2016-04-16 23:45:05 +0000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require "csv"
-CSV.foreach('db/diamond2.csv') do |row|
-  Diamond.create(:date => row[0], :weight => row[1], :color => row[2], :clar => row[3], :cut_grade => row[4], 
-                :rapnet_list_price => row[5], :rapnet_discount => row[6], :price_per_carat => row[7], 
-                :polish => row[8], :symmetry => row[9], :fluorescen => row[10], :certificate_id => row[11], :end_price => row[12])
+CSV.foreach('db/list3.csv') do |row|
+  # Diamond.create(:date => row[0], :weight => row[1], :color => row[2], :clar => row[3], :cut_grade => row[4], 
+  #               :rapnet_list_price => row[5], :rapnet_discount => row[6], :price_per_carat => row[7], 
+  #               :polish => row[8], :symmetry => row[9], :fluorescen => row[10], :certificate_id => row[11], :end_price => row[12])
+                
+  Diamond.create(:date => row[0], :weight => row[4], :color => row[5], :clar => row[9], :length => row[10], :width => row[11],
+                          :depth => row[12], :cut_grade => row[14], :rapnet_list_price => row[16], :rapnet_discount => row[17], :price_per_carat => row[18], 
+                        :polish => row[26], :symmetry => row[27], :fluorescen => row[28], :certificate_id => row[37], :end_price => row[48])
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
