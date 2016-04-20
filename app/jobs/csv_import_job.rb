3654b392a71f69a35f1d77c4971c718088d1463d
sunjewelde <shibamuran@gmail.com>
2016-04-20 21:03:16 +0000

    t

--- app/jobs/csv_import_job.rb
+++ app/jobs/csv_import_job.rb
@@ -8,8 +8,8 @@ class CsvImportJob < ActiveJob::Base
                 # CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
                 # chunk.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
                 
-            
-            chunk.each do |row|
+            chunk.each do |key, value|
+            # chunk.each do |row|
                 # hash.each do |row|
                 #     diamond = Diamond.new
                     
@@ -55,9 +55,7 @@ class CsvImportJob < ActiveJob::Base
                 #         :polish => row[26], :symmetry => row[27], :fluorescen => row[28], :certificate_id => row[37], :end_price => row[48])
                 # end   
                 
-                Diamond.create(:date => row[0], :weight => row[4], :color => row[5], :clar => row[9], :length => row[10], :width => row[11],
-                         :depth => row[12], :cut_grade => row[14], :rapnet_list_price => row[16], :rapnet_discount => row[17], :price_per_carat => row[18], 
-                        :polish => row[26], :symmetry => row[27], :fluorescen => row[28], :certificate_id => row[37], :end_price => row[48])
+                Diamond.create(:date => value[0], :weight => value[4], :color => value[5], :clar => value[9], :length => value[10], :width => value[11])
                 # end  
                 
         end
