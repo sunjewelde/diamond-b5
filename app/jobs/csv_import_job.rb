caa55875544ec389b283932f5885ba7b917c7f3a
sunjewelde <shibamuran@gmail.com>
2016-04-20 20:43:43 +0000

    test

--- app/jobs/csv_import_job.rb
+++ app/jobs/csv_import_job.rb
@@ -58,7 +58,7 @@ class CsvImportJob < ActiveJob::Base
                 Diamond.create(:date => row[0], :weight => row[4], :color => row[5], :clar => row[9], :length => row[10], :width => row[11],
                          :depth => row[12], :cut_grade => row[14], :rapnet_list_price => row[16], :rapnet_discount => row[17], :price_per_carat => row[18], 
                         :polish => row[26], :symmetry => row[27], :fluorescen => row[28], :certificate_id => row[37], :end_price => row[48])
-                end  
+                # end  
                 
         end
                     
