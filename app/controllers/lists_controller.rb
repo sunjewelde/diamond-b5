af8da7eb0a96cfc4a5fe6f0c1bd1066ccdf33fd3
sunjewelde <shibamuran@gmail.com>
2016-04-23 11:39:47 +0000

    t

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,7 +31,7 @@ class ListsController < ApplicationController
     if List.exists?(date: selected_date)
       @list_selected_date = selected_date
     else
-      @list_selected_date = @latest_date
+      @list_selected_date = @list_latest_date
       flash.now[:error] = "日付が選択されていない、又はその日付のデータがありません。"
     end
     
