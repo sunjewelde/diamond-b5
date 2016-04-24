a90248735043eb1011431ed99b775d95b62d03d8
sunjewelde <shibamuran@gmail.com>
2016-04-24 01:52:09 +0000

    t

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List <ActiveRecord::Base
     
-    scope :select_color_clar, -> {select('color, if1, vvs1, vvs2, vs1, vs2, si1, si2')}
+    scope :select_color_clar, -> lambda {select('color, if1, vvs1, vvs2, vs1, vs2, si1, si2')}
     
     
 end
