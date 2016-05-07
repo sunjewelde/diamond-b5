72873a070a47e525dd042f68345786fafe4836aa
sunjewelde <shibamuran@gmail.com>
2016-05-07 00:24:19 +0000

    add x-Frame-option

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module Code3
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :sidekiq
+    config.action_dispatch.default_headers['X-Frame-Options'] = "Allow-From http://www.diamond-bourse.com/"
   end
 end
