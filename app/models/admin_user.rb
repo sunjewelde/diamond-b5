7d30063c353ebed52ab73a619d9e7b5bbe4f9694
sunjewelde <shibamuran@gmail.com>
2016-05-12 03:45:02 +0000

    added require_user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,4 @@
 class AdminUser < ActiveRecord::Base
-  before_action :require_user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
