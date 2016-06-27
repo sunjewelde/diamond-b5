61b5f2962c8937ce231b78692062edbddd7b3f67
sunjewelde <shibamuran@gmail.com>
2016-06-27 23:49:19 +0000

    t

--- app/jobs/organize_lists_job.rb
+++ app/jobs/organize_lists_job.rb
@@ -5,7 +5,8 @@ class OrganizeListsJob < ActiveJob::Base
   def perform(*args)
     # Do something later
       # diamond_date = Diamond.pluck(:date).uniq
-      diamond_date = Diamond.find_each(batch_size: 10000).map(&:date).uniq
+      diamond_date = Table.find_each.map(&:date).uniq
+      # diamond_date = Diamond.find_each(batch_size: 10000).map(&:date).uniq
       table_date = List.pluck(:date).uniq
       # uniq_date_pre = diamond_date - table_date
       # uniq_date = uniq_date_pre.compact.sort {|a, b| b <=> a }
