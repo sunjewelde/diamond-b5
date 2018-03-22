a2b6726b3e1891c105d8a8c4c6f318533505e29a
sunjewelde <shibamuran@gmail.com>
2018-03-22 05:10:51 +0000

    ttt

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,9 @@ set :deploy_to, '/var/www/myapp'
 set :pty, false
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('log')
+set :keep_releases, 5
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
